32d20cdd9f9258f51458c80c0c6eff7885484bed
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-08-18 15:07:42 +0000

    Configure cli setup argument

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 
+const setup = require("../lib/setup.js");
+
 const [, , ...args] = process.argv;
 
-console.log(`Hello World ${args}`);
+switch (args[0]) {
+  case "clean":
+    console.log("run clean");
+    break;
+  case "dev":
+    console.log("run dev");
+    break;
+  case "extract":
+    console.log("run extract");
+    break;
+  case "open":
+    console.log("run open");
+    break;
+  case "setup":
+    setup();
+    break;
+  case "update":
+    console.log("run update");
+    break;
+  default:
+    console.log(`Hello World ${args}`);
+}
